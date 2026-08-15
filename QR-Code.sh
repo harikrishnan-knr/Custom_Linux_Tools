9c3283e882e706d7e6f1f3d893c6d9377f6069f6
hari <harikrishnanknr07@gmail.com>
2026-08-15 13:32:32 +0530

    qrcode

--- QR-Code.sh
+++ QR-Code.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-echo "QR Code Generator Tool"
-echo "Is This OK To Install The Tool [y/n]: "
+green='\033[32m'
+red='\033[31m'
+nc='\033[0m'
+
+echo "${green}QR Code Generator Tool${nc}"
+echo "${green}Is This OK To Install The Tool [y/n]: ${nc}"
 read type
 
 if [ $type == "y" ] || [ $type == "Y" ]; then
@@ -13,9 +17,9 @@ sudo chmod +x /usr/local/bin/qrcode
 
 elif [ $type == "n" ] || [ $type == "N" ]; then
 # Script Not Writing and Exit the Installation Process...
-echo "Exit the Installation Process..."
+echo "${red}Exit the Installation Process...${nc}"
 
 else
-echo "Re-run The Script Again & Enter Correct Value..."
+echo "${red}Re-run The Script Again & Enter Correct Value...${nc}"
 
 fi
